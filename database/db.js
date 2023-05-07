cbce08638731d67ee8400b52fea09eefb0165446
Christian Stemmann <stemmi@sanktemmi.de>
2023-05-07 13:48:18 +0200

    Revert "no message"
    This reverts commit 09c2562e8b9010b93ee960d141bf4c88bb961c91.

--- database/db.js
+++ database/db.js
@@ -5,8 +5,7 @@ const connection = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    timezone: "UTC"
+    database: process.env.DB_DATABASE
 });
 
 function query(sql, params = []) {
