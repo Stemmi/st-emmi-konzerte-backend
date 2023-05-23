66fd85274e4110bc515dcc8d2383bcc7d726c944
Christian Stemmann <stemmi@sanktemmi.de>
2023-05-23 17:36:49 +0200

    fix: date validation

--- services/sanitizer.js
+++ services/sanitizer.js
@@ -2,13 +2,13 @@ const validator = require('validator');
 
 function healShow(data) {
     try {
-        const date = data.date ? validator.toDate(data.date) : null;
+        const date = validator.toDate(data.date);
         const location_id = validator.toInt(data.location_id);
         const user_id = validator.toInt(data.user_id);
         return {
             title: text255(data.title) || null,
             location_id: location_id,
-            date: validator.isDate(date) ? date : null,
+            date: date,
             text: text255(data.text) || null,
             poster_filename: text255(data.poster_filename) || null,
             poster_alt: text255(data.poster_alt) || null,
