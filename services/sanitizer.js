a10c1f934d67621539a680225c3d1028e473bcf4
Christian Stemmann <stemmi@sanktemmi.de>
2023-05-30 19:00:22 +0200

    sanitizer update - fix: always use string input, feat: bands

--- services/sanitizer.js
+++ services/sanitizer.js
@@ -1,27 +1,29 @@
 const validator = require('validator');
 
 function healShow(data) {
-    const date = validator.toDate(data.date);
-    const location_id = validator.toInt(data.location_id);
-    const user_id = validator.toInt(data.user_id);
+    const date = validator.toDate(data.date+'');
+    const location_id = validator.toInt(data.location_id+'');
+    const user_id = validator.toInt(data.user_id+'');
+    const bands = data.bands ? data.bands.map((band) => validator.toInt(band+'')) : [];
     return {
-        title: text255(data.title) || null,
+        title: text255(data.title+'') || null,
         location_id: location_id,
         date: date,
-        text: text255(data.text) || null,
-        poster_filename: text255(data.poster_filename) || null,
-        poster_alt: text255(data.poster_alt) || null,
+        text: text255(data.text+'') || null,
+        poster_filename: text255(data.poster_filename+'') || null,
+        poster_alt: text255(data.poster_alt+'') || null,
+        bands: bands,
         user_id: user_id
     }
 }
 
 function healLocation(data) {
-    const lat = validator.toFloat(data.lat);
-    const long = validator.toFloat(data.long);
+    const lat = validator.toFloat(data.lat+'');
+    const long = validator.toFloat(data.long+'');
     return {
-        name: text255(data.name) || null,
-        city: text255(data.city) || null,
-        url: url255(data.url) || null,
+        name: text255(data.name+'') || null,
+        city: text255(data.city+'') || null,
+        url: url255(data.url+'') || null,
         lat: lat,
         long: long
     }
@@ -29,15 +31,16 @@ function healLocation(data) {
 
 function healBand(data) {
     return {
-        name: text255(data.name) || null,
-        url: url255(data.url) || null
+        name: text255(data.name+'') || null,
+        url: url255(data.url+'') || null
     }
 }
 
 function text255(text) {
     if (!text) return null;
-    const trimmed = validator.trim(text);
-    const escaped = validator.escape(trimmed);
+    const trimmed = validator.trim(text+'');
+    const cleaned = trimmed.replace(/([\/|\\]|"|&|´|`|{|}|<|>|')/g, "");
+    const escaped = validator.escape(cleaned);
     const limited = escaped.slice(0, 255);
     return limited;
 }
